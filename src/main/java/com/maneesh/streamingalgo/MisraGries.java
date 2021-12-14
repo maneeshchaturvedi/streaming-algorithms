8fde71fec23a7edc6db3a833b5561332c0e8cefc
maneesh.chaturvedi <maneesh.chaturvedi@gmail.com>
2021-12-14 20:17:42 +0530

    Misra Gries Frequency Algorithm

---
+++ src/main/java/com/maneesh/streamingalgo/MisraGries.java
@@ -0,0 +1,60 @@
+package com.maneesh.streamingalgo;
+
+import java.util.*;
+
+public class MisraGries {
+    private final Map<Integer, Integer> associativeArr;
+    private final int k;
+    private final Integer[] sequence;
+
+    public MisraGries(Integer[] sequence, int k) {
+        associativeArr = new HashMap<>();
+        this.k = k;
+        this.sequence = sequence;
+        populateAssociativeArr();
+    }
+
+    public Set<Integer> keys() {
+        return associativeArr.keySet();
+    }
+    public Integer query(Integer element) {
+        if (associativeArr.containsKey(element)) {
+            return freq(element);
+        }
+        return null;
+    }
+
+    private Integer freq(Integer element) {
+        Integer freq = 0;
+        for(Integer x : sequence ){
+            if(x.equals(element)){
+                freq++;
+            }
+        }
+        return freq;
+    }
+
+    private void populateAssociativeArr() {
+        if (sequence == null || sequence.length == 0) return;
+        for (int element : sequence) {
+            if (associativeArr.containsKey(element)) {
+                int elementCounter = associativeArr.get(element);
+                associativeArr.put(element, elementCounter + 1);
+            } else if (associativeArr.size() < k)  {
+                associativeArr.put(element, 1);
+            } else {
+                Iterator<Integer> iter = associativeArr.keySet().iterator();
+                while (iter.hasNext()) {
+                    Integer currentKey = iter.next();
+                    Integer currentCount = associativeArr.get(currentKey);
+                    associativeArr.put(currentKey, currentCount - 1);
+                    if (associativeArr.get(currentKey) == 0) {
+                        iter.remove();
+                    }
+                }
+            }
+        }
+    }
+
+
+}
---
+++ src/test/java/com/maneesh/streamingalgo/MisraGriesTest.java
@@ -0,0 +1,73 @@
+package com.maneesh.streamingalgo;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MisraGriesTest {
+
+    @Test
+    void query_Top2FreqElements() {
+        Integer [] sequence =  new Integer[]{1,5,2,4,2,4,3,4,2,4,5,4,5,3,2,5,4,5,5,4};
+        int k = 2;
+        MisraGries mg = new MisraGries(sequence, k);
+
+        Set<Integer> keys = mg.keys();
+        assertEquals(keys.size(), 2);
+        assertTrue(keys.contains(4));
+        assertTrue(keys.contains(5));
+        assertFalse(keys.contains(2));
+        assertFalse(keys.contains(3));
+        assertFalse(keys.contains(1));
+
+        
+        Integer freq = mg.query(4);
+        assertEquals(freq, 7);
+        freq = mg.query(5);
+        assertEquals(freq, 6);
+
+        freq = mg.query(2);
+        assertNull(freq);
+
+        freq = mg.query(3);
+        assertNull(freq);
+
+        freq = mg.query(1);
+        assertNull(freq);
+
+
+    }
+    @Test
+    void query_Top4FreqElements() {
+        Integer [] sequence =  new Integer[]{1,5,2,4,2,4,3,4,2,4,5,4,5,3,2,5,4,5,5,4};
+        int k = 4;
+        MisraGries mg = new MisraGries(sequence, k);
+
+        Set<Integer> keys = mg.keys();
+        assertEquals(keys.size(), 4);
+        assertTrue(keys.contains(4));
+        assertTrue(keys.contains(5));
+        assertTrue(keys.contains(2));
+        assertTrue(keys.contains(3));
+        assertFalse(keys.contains(1));
+
+        Integer freq = mg.query(4);
+        assertEquals(freq, 7);
+        freq = mg.query(5);
+        assertEquals(freq, 6);
+        freq = mg.query(2);
+        assertEquals(freq,4);
+
+        freq = mg.query(3);
+        assertEquals(freq,2);
+
+        freq = mg.query(1);
+        assertNull(freq);
+
+
+    }
+
+}
